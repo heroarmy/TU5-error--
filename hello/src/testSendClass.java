578735647eab400a03ba3b49c12753babbb88cae
MacBook Pro <MacBook Pro@computer_1>
2012-09-22 17:10:43 +0700

    testSendClass

---
+++ hello/src/testSendClass.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MacBook Pro
+ */
+public class testSendClass {
+    
+}
